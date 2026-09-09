2cbbd22c20f491da5692487e42fdd944ba95a390
thach-le <lehoangthach2k2@gmail.com>
2026-09-09 21:51:07 +0700

    1 choi 19

--- supabase.js
+++ supabase.js
@@ -1,19 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
-if (!supabaseUrl) {
-  console.error("SUPABASE_URL chưa được cấu hình");
-}
-
-if (!supabaseKey) {
-  console.error("SUPABASE_ANON_KEY chưa được cấu hình");
-}
-
 const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 module.exports = supabase;
