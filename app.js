8c7a71aed6fa197cb983372778368e4a2931fd32
thach-le <lehoangthach2k2@gmail.com>
2026-09-09 18:44:54 +0700

    up code backend

--- app.js
+++ app.js
@@ -2,23 +2,16 @@ const express = require("express");
 
 const app = express();
 
-
-// Middleware
 app.use(express.json());
 
-
-// Routes
 const authRoutes = require("./routes/auth");
 
 app.use("/api/auth", authRoutes);
 
-
-// Test server
 app.get("/", (req, res) => {
   res.json({
     message: "Backend is running"
   });
 });
-
 
 module.exports = app;
