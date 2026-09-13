3ba48cc25ff283064d942df69af97f5a53e24f50
thach-le <lehoangthach2k2@gmail.com>
2026-09-13 17:00:56 +0700

    edit name key in env

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.MY_SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Thiếu biến môi trường SUPABASE_URL hoặc SUPABASE_ANON_KEY");
