e8e3ce1d612ecfc8402267b530759c66f886a2d3
thach-le <lehoangthach2k2@gmail.com>
2026-09-13 17:09:33 +0700

    edit cònig supbase

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  process.env.SUPABASE_URL + "/rest/v1",
   process.env.MY_SUPABASE_KEY
 );
 
