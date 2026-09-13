c912aed8a6d41c980f358064f911509bb47d527b
thach-le <lehoangthach2k2@gmail.com>
2026-09-13 17:14:45 +0700

    edit cònig supbase

--- supabase.js
+++ supabase.js
@@ -1,12 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.MY_SUPABASE_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Thiếu biến môi trường SUPABASE_URL hoặc MY_SUPABASE_KEY");
-}
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.MY_SUPABASE_KEY
+);
 
 module.exports = supabase;
